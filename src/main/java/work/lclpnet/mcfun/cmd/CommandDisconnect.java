78748d6d45d0aec7302972f4eb3c2a3e8b77c804
DerBerliner <34981166+DerBerlinr@users.noreply.github.com>
2021-05-12 21:45:18 +0200

    LivingEntityFix

--- src/main/java/work/lclpnet/mcfun/cmd/CommandDisconnect.java
+++ src/main/java/work/lclpnet/mcfun/cmd/CommandDisconnect.java
@@ -6,6 +6,7 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.TranslatableText;
@@ -42,22 +43,30 @@ public class CommandDisconnect extends CommandBase {
 
         if(entity1.equals(entity2)) throw ENTITIES_EQUAL.create();
 
-        IRopeConnectable conn1 = IRopeConnectable.getFrom(entity1);
-        IRopeConnectable conn2 = IRopeConnectable.getFrom(entity2);
+        if(!(entity1 instanceof LivingEntity))
+            throw new SimpleCommandExceptionType(new TranslatableText("commands.connect.entities.not-living", entity1.getEntityName())).create();
+        if(!(entity2 instanceof LivingEntity))
+            throw new SimpleCommandExceptionType(new TranslatableText("commands.connect.entities.not-living", entity2.getEntityName())).create();
 
-        if(!conn1.isConnectedTo(entity2) && !conn2.isConnectedTo(entity1)) throw ENTITIES_NOT_LINKED.create();
+        LivingEntity le1 = (LivingEntity) entity1;
+        LivingEntity le2 = (LivingEntity) entity2;
+
+        IRopeConnectable conn1 = IRopeConnectable.getFrom(le1);
+        IRopeConnectable conn2 = IRopeConnectable.getFrom(le2);
+
+        if(!conn1.isConnectedTo(le2) && !conn2.isConnectedTo(le1)) throw ENTITIES_NOT_LINKED.create();
 
         Optional<Rope> e1toe2 = Objects.requireNonNull(conn1.getRopeConnections()).stream().filter(rope -> rope.getConnectedTo().equals(entity2)).findFirst();
         Optional<Rope> e2toe1 = Objects.requireNonNull(conn2.getRopeConnections()).stream().filter(rope -> rope.getConnectedTo().equals(entity1)).findFirst();
 
         if (e1toe2.isPresent()) {
             Rope rope = e1toe2.get();
-            conn1.removeRopeConnection(rope);
+            conn1.removeRopeConnection(rope, true);
         }
 
         if (e2toe1.isPresent()) {
             Rope rope2 = e2toe1.get();
-            conn2.removeRopeConnection(rope2);
+            conn2.removeRopeConnection(rope2, true);
         }
 
         MutableText feedback = new TranslatableText("commands.disconnect.entities.disconnected", entity1.getName(), entity2.getName()).formatted(Formatting.RED);
